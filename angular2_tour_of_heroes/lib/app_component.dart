2f48833962afd044ca005dd5716b52b223d7ddbc
Michael Gill <somdev1@mail.ucsd.edu>
2016-10-21 15:38:45 -0700

    Step: Serveces, before Return Mocked Heroes

--- angular2_tour_of_heroes/lib/app_component.dart
+++ angular2_tour_of_heroes/lib/app_component.dart
@@ -69,22 +69,9 @@ import 'hero_detail_component.dart';
 class AppComponent {
   String title = 'Tour of Heroes';
   Hero selectedHero;
-  final List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
 
   onSelect(Hero hero) {
     selectedHero = hero;
   }
 }
-
-final List<Hero> mockHeroes = [
-  new Hero(11, 'Mr. Nice'),
-  new Hero(12, 'Narco'),
-  new Hero(13, 'Bombasto'),
-  new Hero(14, 'Celeritas'),
-  new Hero(15, 'Magneta'),
-  new Hero(16, 'RubberMan'),
-  new Hero(17, 'Dynama'),
-  new Hero(18, 'Dr IQ'),
-  new Hero(19, 'Magma'),
-  new Hero(20, 'Tornado')
-];
